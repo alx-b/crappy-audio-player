661a5083ad72bd29f24ce23ff34402fb42e0bf54
alx-b <>
2021-07-16 11:58:22 +0200

    Add functions to deal with audio files, metadata, sorting, etc.

---
+++ crappy_audio_player/audio.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+import pathlib
+import dataclasses
+
+import pygame
+import mutagen
+
+
+@dataclasses.dataclass
+class File:
+    path: pathlib.PosixPath or str
+    metadata: dict
+
+    def __str__(self) -> str:
+        new_meta = {}
+        for data in self.metadata:
+            new_meta.update({data: "".join(self.metadata[data])})
+
+        return (
+            f"{new_meta.get('artist', '')} - {new_meta.get('date', '')}"
+            + f" - {new_meta.get('album', '')} - {new_meta.get('tracknumber', '')}"
+            + f" - {new_meta.get('title', '')}"
+        )
+
+
+def get_supported_audio_files() -> list[File]:
+    try:
+        return _sort_audio_files(
+            [
+                File(filepath, _get_metadata(filepath))
+                for filepath in _get_filepaths(f"{pathlib.Path.home()}/Music")
+                if _is_file_loadable(filepath)
+            ]
+        )
+    except Exception:
+        return "Something went wrong"
+
+
+def _sort_audio_files(files: list[File]) -> list[File]:
+    # Sort by Artist, date, and then tracknumber.
+    return sorted(
+        files,
+        key=lambda file: (
+            "".join(file.metadata.get("artist")).lower(),
+            "".join(file.metadata.get("date")),
+            int("".join(file.metadata.get("tracknumber", "0"))),
+        ),
+    )
+
+
+def _get_metadata(filepath: pathlib.PosixPath or str):
+    try:
+        return mutagen.File(filepath, options=None, easy=True)
+    except Exception:
+        return f"cant read metadata - {filepath}"
+
+
+def _get_filepaths(main_path: str) -> list[pathlib.PosixPath]:
+    music_path = pathlib.Path(main_path)
+    return [path for path in list(music_path.glob("**/*")) if path.is_file()]
+
+
+def _is_file_loadable(filepath: pathlib.PosixPath or str) -> bool:
+    try:
+        pygame.mixer.music.load(filepath)
+        return True
+    except Exception:
+        return False
+
+
+pygame.mixer.init()
